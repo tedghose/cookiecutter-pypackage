bf22b9f97cf19c0278830f78aac3e3dc536636ce
tedghose <rintu@hotmail.com>
2025-02-08 11:23:07 -0800

    main file with MVC default

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,1 +1,24 @@
 """Main module."""
+
+class Model:
+    def __init__(self, args):
+        self.args = args
+
+    def ctrlr(self, ctrlr=None):
+        if ctrlr is not None:
+            self.controler = ctrlr
+        return self.controler
+
+class View:
+    def __init__(self, ctrlr):
+        self.ctrlr = ctrlr
+
+
+class Controller(TaskController):
+    def __init__(self, model):
+        self.model = model
+        self.model.ctrlr(self)
+        self.view = View(self)
+
+    def run(self):
+        return 0
